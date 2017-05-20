565a608a7fd45a2c7b6cc6fedca91fb18a53ce76
Gürhan Küçük <gurhan.kucuk@yandex.com>
2017-05-20 19:05:47 +0300

    added score type service.

---
+++ src/main/java/com/kayra/university_preference_guide/service/ScoreTypeService.java
@@ -0,0 +1,10 @@
+package com.kayra.university_preference_guide.service;
+
+import java.util.List;
+
+import com.kayra.university_preference_guide.model.ScoreType;
+
+public interface ScoreTypeService {
+
+	public List<ScoreType> getScoreTypes();
+}
---
+++ src/main/java/com/kayra/university_preference_guide/service/imp/ScoreTypeServiceImpl.java
@@ -0,0 +1,16 @@
+package com.kayra.university_preference_guide.service.imp;
+
+import java.util.List;
+
+import com.kayra.university_preference_guide.model.ScoreType;
+import com.kayra.university_preference_guide.service.ScoreTypeService;
+
+public class ScoreTypeServiceImpl implements ScoreTypeService {
+
+	@Override
+	public List<ScoreType> getScoreTypes() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
